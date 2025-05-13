34df1c3f5c52cd30bb185ad2b7466aa2d6b5b0bf
Onyekachi Eze <ezeonyekachipaul@gmail.com>
2025-05-13 15:12:47 +0100

    removed inline

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "http://localhost:5000/api/",
-    withCredentials: true, // send cookies with requests
+    withCredentials: true,
     
 });
